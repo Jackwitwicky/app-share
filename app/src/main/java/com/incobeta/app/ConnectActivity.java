4af5267e8c59cf6f372ab282fdf446f500ed7e00
Jackwitwicky <jacknkiarie@gmail.com>
2020-02-26 07:32:12 +0300

    added changes to attempt one

--- app/src/main/java/com/incobeta/app/ConnectActivity.java
+++ app/src/main/java/com/incobeta/app/ConnectActivity.java
@@ -1,11 +1,24 @@
 package com.incobeta.app;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,18 +28,38 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.incobeta.app.db.DBAdapter;
 
-public class ConnectActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectActivity extends AppCompatActivity implements PeerListFragment.OnListFragmentInteractionListener
+        , WifiP2pManager.PeerListListener, WifiP2pManager.ConnectionInfoListener{
 
     //global variables
-    WifiP2pManager manager;
-    WifiP2pManager.Channel channel;
-    BroadcastReceiver receiver;
-
-    IntentFilter intentFilter;
 
     RelativeLayout connectInstructionsLayout;
     LottieAnimationView animationView;
+
+
+    public static final String FIRST_DEVICE_CONNECTED = "first_device_connected";
+    public static final String KEY_FIRST_DEVICE_IP = "first_device_ip";
+
+    private static final String WRITE_PERMISSION = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+    private static final int WRITE_PERM_REQ_CODE = 19;
+
+    PeerListFragment deviceListFragment;
+    View progressBarLocalDash;
+
+    WifiP2pManager wifiP2pManager;
+    WifiP2pManager.Channel wifip2pChannel;
+    WiFiDirectReceiver wiFiDirectBroadcastReceiver;
+    private boolean isWifiP2pEnabled = false;
+
+    private boolean isWDConnected = false;
+
+    private AppController appController;
+//    private ConnectionListener connListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,30 +74,111 @@ public class ConnectActivity extends AppCompatActivity {
         connectInstructionsLayout = (RelativeLayout) findViewById(R.id.connectInstructionsLayout);
         animationView = (LottieAnimationView) findViewById(R.id.animation_view);
 
-        //define the network functionality
-        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        channel = manager.initialize(this, getMainLooper(), null);
-        receiver = new WifiDirectBroadcastReceiver(manager, channel, this);
-
-        intentFilter = new IntentFilter();
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+        initialize();
     }
 
+    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled) {
+        this.isWifiP2pEnabled = isWifiP2pEnabled;
+    }
+
+    private void initialize() {
+
+        progressBarLocalDash = findViewById(R.id.progressBarLocalDash);
+
+        String myIP = Utility.getWiFiIPAddress(this);
+        Utility.saveString(this, TransferConstants.KEY_MY_IP, myIP);
+
+//        Starting connection listener with default for now
+//        connListener = new ConnectionListener(LocalDashWiFiDirect.this, TransferConstants.INITIAL_DEFAULT_PORT);
+//        connListener.start();
+
+
+        wifiP2pManager = (WifiP2pManager) getSystemService(WIFI_P2P_SERVICE);
+        wifip2pChannel = wifiP2pManager.initialize(this, getMainLooper(), null);
+
+        // Starting connection listener with default port for now
+        appController = (AppController) getApplicationContext();
+        appController.startConnectionListener(TransferConstants.INITIAL_DEFAULT_PORT);
+
+        checkWritePermission();
+    }
+
+
+    public void findPeers() {
+
+        if (!isWDConnected) {
+            wifiP2pManager.discoverPeers(wifip2pChannel, new WifiP2pManager.ActionListener() {
+                @Override
+                public void onSuccess() {
+                    NotificationToast.showToast(ConnectActivity.this, "Peer discovery started");
+                }
+
+                @Override
+                public void onFailure(int reasonCode) {
+                    NotificationToast.showToast(ConnectActivity.this, "Peer discovery failure: "
+                            + reasonCode);
+                }
+            });
+        }
+        else {
+            Toast.makeText(ConnectActivity.this, "Already Connected", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        registerReceiver(receiver, intentFilter);
+        IntentFilter localFilter = new IntentFilter();
+        localFilter.addAction(DataHandler.DEVICE_LIST_CHANGED);
+        localFilter.addAction(FIRST_DEVICE_CONNECTED);
+        localFilter.addAction(DataHandler.CHAT_REQUEST_RECEIVED);
+        localFilter.addAction(DataHandler.CHAT_RESPONSE_RECEIVED);
+        LocalBroadcastManager.getInstance(ConnectActivity.this).registerReceiver(localDashReceiver,
+                localFilter);
+
+        IntentFilter wifip2pFilter = new IntentFilter();
+        wifip2pFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        wifip2pFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        wifip2pFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        wifip2pFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        wiFiDirectBroadcastReceiver = new WiFiDirectReceiver(wifiP2pManager,
+                wifip2pChannel, this);
+        registerReceiver(wiFiDirectBroadcastReceiver, wifip2pFilter);
+
+        LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(DataHandler.DEVICE_LIST_CHANGED));
     }
 
     @Override
     protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(localDashReceiver);
+        unregisterReceiver(wiFiDirectBroadcastReceiver);
+
         super.onPause();
-        unregisterReceiver(receiver);
+    }
+
+    @Override
+    protected void onDestroy() {
+//        mNsdHelper.tearDown();
+//        connListener.tearDown();
+        appController.stopConnectionListener();
+        Utility.clearPreferences(ConnectActivity.this);
+        Utility.deletePersistentGroups(wifiP2pManager, wifip2pChannel);
+        DBAdapter.getInstance(ConnectActivity.this).clearDatabase();
+        wifiP2pManager.removeGroup(wifip2pChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onFailure(int i) {
+
+            }
+        });
+
+        super.onDestroy();
     }
 
     @Override
@@ -92,6 +206,8 @@ public class ConnectActivity extends AppCompatActivity {
                         connectInstructionsLayout.setVisibility(View.INVISIBLE);
 
                         animationView.setVisibility(View.VISIBLE);
+
+                        findPeers();
                     }
                     else {
                         Toast.makeText(this, "Not connected to any wifi. Ensure Step 2 is complete", Toast.LENGTH_LONG).show();
@@ -102,6 +218,189 @@ public class ConnectActivity extends AppCompatActivity {
                 }
                 break;
 
+        }
+    }
+
+
+    //other necessary code for connection
+
+    private BroadcastReceiver localDashReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()) {
+                case FIRST_DEVICE_CONNECTED:
+//                    connListener.tearDown();
+//                    int newPort = ConnectionUtils.getPort(LocalDashWiFiDirect.this);
+//                    connListener = new ConnectionListener(LocalDashWiFiDirect.this,
+//                            newPort);
+//                    connListener.start();
+//                    appController.stopConnectionListener();
+//                    appController.startConnectionListener(ConnectionUtils.getPort(LocalDashWiFiDirect.this));
+                    appController.restartConnectionListenerWith(ConnectionUtils.getPort(ConnectActivity.this));
+
+                    String senderIP = intent.getStringExtra(KEY_FIRST_DEVICE_IP);
+                    int port = DBAdapter.getInstance(ConnectActivity.this).getDevice
+                            (senderIP).getPort();
+                    DataSender.sendCurrentDeviceData(ConnectActivity.this, senderIP, port, true);
+                    isWDConnected = true;
+                    break;
+                case DataHandler.DEVICE_LIST_CHANGED:
+                    ArrayList<DeviceDTO> devices = DBAdapter.getInstance(ConnectActivity.this)
+                            .getDeviceList();
+                    int peerCount = (devices == null) ? 0 : devices.size();
+                    if (peerCount > 0) {
+                        progressBarLocalDash.setVisibility(View.GONE);
+                        deviceListFragment = new PeerListFragment();
+                        Bundle args = new Bundle();
+                        args.putSerializable(PeerListFragment.ARG_DEVICE_LIST, devices);
+                        deviceListFragment.setArguments(args);
+
+                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                        ft.replace(R.id.deviceListHolder, deviceListFragment);
+                        ft.setTransition(FragmentTransaction.TRANSIT_NONE);
+                        ft.commit();
+                    }
+
+                    break;
+                case DataHandler.CHAT_REQUEST_RECEIVED:
+                    DeviceDTO chatRequesterDevice = (DeviceDTO) intent.getSerializableExtra(DataHandler
+                            .KEY_CHAT_REQUEST);
+                    DialogUtils.getChatRequestDialog(ConnectActivity.this,
+                            chatRequesterDevice).show();
+                    break;
+                case DataHandler.CHAT_RESPONSE_RECEIVED:
+                    boolean isChatRequestAccepted = intent.getBooleanExtra(DataHandler
+                            .KEY_IS_CHAT_REQUEST_ACCEPTED, false);
+                    if (!isChatRequestAccepted) {
+                        NotificationToast.showToast(ConnectActivity.this, "Chat request " +
+                                "rejected");
+                    } else {
+                        DeviceDTO chatDevice = (DeviceDTO) intent.getSerializableExtra(DataHandler
+                                .KEY_CHAT_REQUEST);
+//                        DialogUtils.openChatActivity(LocalDashWiFiDirect.this, chatDevice);
+                        NotificationToast.showToast(ConnectActivity.this, chatDevice
+                                .getPlayerName() + "Accepted Chat request");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    private DeviceDTO selectedDevice;
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case DialogUtils.CODE_PICK_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    Uri imageUri = data.getData();
+                    DataSender.sendFile(ConnectActivity.this, selectedDevice.getIp(),
+                            selectedDevice.getPort(), imageUri);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+            finish();
+        }
+    }
+
+    private void checkWritePermission() {
+        boolean isGranted = Utility.checkPermission(WRITE_PERMISSION, this);
+        if (!isGranted) {
+            Utility.requestPermission(WRITE_PERMISSION, WRITE_PERM_REQ_CODE, this);
+        }
+    }
+
+    boolean isConnectionInfoSent = false;
+
+
+    @Override
+    public void onConnectionInfoAvailable(WifiP2pInfo wifiP2pInfo) {
+
+        if (wifiP2pInfo.groupFormed && !wifiP2pInfo.isGroupOwner && !isConnectionInfoSent) {
+
+            isWDConnected = true;
+
+//            connListener.tearDown();
+//            connListener = new ConnectionListener(LocalDashWiFiDirect.this, ConnectionUtils.getPort
+//                    (LocalDashWiFiDirect.this));
+//            connListener.start();
+//            appController.stopConnectionListener();
+//            appController.startConnectionListener(ConnectionUtils.getPort(LocalDashWiFiDirect.this));
+            appController.restartConnectionListenerWith(ConnectionUtils.getPort(ConnectActivity.this));
+
+            String groupOwnerAddress = wifiP2pInfo.groupOwnerAddress.getHostAddress();
+            DataSender.sendCurrentDeviceDataWD(ConnectActivity.this, groupOwnerAddress, TransferConstants
+                    .INITIAL_DEFAULT_PORT, true);
+            isConnectionInfoSent = true;
+        }
+    }
+
+    @Override
+    public void onPeersAvailable(WifiP2pDeviceList peerList) {
+
+        ArrayList<DeviceDTO> deviceDTOs = new ArrayList<>();
+
+        List<WifiP2pDevice> devices = (new ArrayList<>());
+        devices.addAll(peerList.getDeviceList());
+        for (WifiP2pDevice device : devices) {
+            DeviceDTO deviceDTO = new DeviceDTO();
+            deviceDTO.setIp(device.deviceAddress);
+            deviceDTO.setPlayerName(device.deviceName);
+            deviceDTO.setDeviceName(new String());
+            deviceDTO.setOsVersion(new String());
+            deviceDTO.setPort(-1);
+            deviceDTOs.add(deviceDTO);
+        }
+
+
+        progressBarLocalDash.setVisibility(View.GONE);
+        deviceListFragment = new PeerListFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(PeerListFragment.ARG_DEVICE_LIST, deviceDTOs);
+        deviceListFragment.setArguments(args);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.deviceListLayout, deviceListFragment);
+        ft.setTransition(FragmentTransaction.TRANSIT_NONE);
+        ft.commit();
+    }
+
+    @Override
+    public void onListFragmentInteraction(DeviceDTO deviceDTO) {
+        if (!isWDConnected) {
+            WifiP2pConfig config = new WifiP2pConfig();
+            config.deviceAddress = deviceDTO.getIp();
+            config.wps.setup = WpsInfo.PBC;
+            config.groupOwnerIntent = 4;
+            wifiP2pManager.connect(wifip2pChannel, config, new WifiP2pManager.ActionListener() {
+                @Override
+                public void onSuccess() {
+                    // Connection request succeeded. No code needed here
+                }
+
+                @Override
+                public void onFailure(int reasonCode) {
+                    NotificationToast.showToast(ConnectActivity.this, "Connection failed. try" +
+                            " again: reason: " + reasonCode);
+                }
+            });
+        } else {
+            selectedDevice = deviceDTO;
+//            showServiceSelectionDialog();
+            DialogUtils.getServiceSelectionDialog(ConnectActivity.this, deviceDTO).show();
         }
     }
 }
